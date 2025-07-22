4fd7e9bc1d7d184b8fc272f1835ff782b6b69060
Kimjiyeon970608 <ne2210ne@naver.com>
2025-07-22 17:59:30 +0900

    카테고리 수정

--- script/js.js
+++ script/js.js
@@ -1,3 +1,20 @@
 document.querySelector("button").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.Swiper) {
+    new Swiper(".category-swiper", {
+      slidesPerView: 3,
+      spaceBetween: 24,
+      navigation: {
+        nextEl: ".category-swiper .swiper-button-next",
+        prevEl: ".category-swiper .swiper-button-prev",
+      },
+      pagination: {
+        el: ".category-swiper .swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+});
