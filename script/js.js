af71c66168ca273cab2ce105c8125fb0500e0e5c
Kimjiyeon970608 <ne2210ne@naver.com>
2025-07-28 17:28:30 +0900

    product 추가

--- script/js.js
+++ script/js.js
@@ -5,8 +5,6 @@ document.querySelector("button").addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function () {
   if (window.Swiper) {
     new Swiper(".category-swiper", {
-      slidesPerView: 3,
-      spaceBetween: 24,
       navigation: {
         nextEl: ".category-swiper .swiper-button-next",
         prevEl: ".category-swiper .swiper-button-prev",
@@ -14,6 +12,30 @@ document.addEventListener("DOMContentLoaded", function () {
       pagination: {
         el: ".category-swiper .swiper-pagination",
         clickable: true,
+      },
+    });
+  }
+});
+
+// product swiper
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.Swiper) {
+    new Swiper(".product-swiper", {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      navigation: {
+        nextEl: ".product-swiper .swiper-button-next",
+        prevEl: ".product-swiper .swiper-button-prev",
+      },
+      pagination: {
+        el: ".product-swiper .swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: { slidesPerView: 1 },
+        600: { slidesPerView: 2 },
+        900: { slidesPerView: 3 },
+        1200: { slidesPerView: 4 },
       },
     });
   }
